107bb1b98ab5c1d7544a3a36872c7e0088448d47
Melissa Robbins <melissamrobbins@gmail.com>
2020-02-10 10:54:59 -0700

    Add vcr and webmock configs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,17 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'vcr'
+require 'webmock/rspec'
+
+VCR.configure do |config|
+  config.ignore_localhost = true
+  config.cassette_library_dir = 'spec/cassettes'
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+  config.filter_sensitive_data("<POTTER_KEY>") { ENV['POTTER_KEY'] }
+  config.allow_http_connections_when_no_cassette = true
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
